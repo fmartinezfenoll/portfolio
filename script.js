c2a664e05c7f074bc72b44d8f221bb50caa90480
fmartinezfenoll <franmartinezfenoll@gmail.com>
2026-09-03 19:40:26 +0200

    First push of proyect(proyect section unfinished)

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry)=>{
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
